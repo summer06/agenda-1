f7f0e85e86a58aa5c0e6616aa0e13652c8fd8943
Snap <dlb7814@live.com>
2017-10-29 19:55:12 +0800

    add encode.go

---
+++ fileio/encode.go
@@ -0,0 +1,27 @@
+package fileio
+
+import (
+	"encode/json"
+	"fmt"
+	"io/ioutil"
+)
+
+func StructToJson(s interface{}) []byte {
+	b, err := json.Marshal(s)
+	if err != nil {
+		return b
+	}
+}
+
+func ReadFile(filename string) (map[string]interface{}, error) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("ReadFile: ", err.Error())
+		return nil, err
+	}
+	if err := json.Unmarshal(bytes, &xxx); err != nil {
+		fmt.Println("Unmarshal: ", err.Error())
+		return nil, err
+	}
+	return xxx, nil
+}
